4eeab01de4b6851dc957ba4f5f90224d883d9ab5
turupawn <ahmed.hn.43@gmail.com>
2021-07-07 00:00:11 -0600

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+var contract
+var accounts
+var web3
+
+const getHello = async () => {
+  hello = await contract.methods.hello().call()
+  document.getElementById("p_hello").innerHTML = hello;
+}
+
+const setHello = async () => {
+  var hello = document.getElementById('input_hello').value
+  const result = await contract.methods.setHello(hello)
+  .send({ from: accounts[0], gas: 400000 })
+  .catch((revertReason) => {
+    getRevertReason(revertReason.receipt.transactionHash);
+  });
+};
